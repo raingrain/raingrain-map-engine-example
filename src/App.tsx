800936fc7437efbe7ac428a20ffd85fd8f0165e7
raingrain <2711835261@qq.com>
2023-11-26 19:58:21 +0800

    示例数据更新

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
+// import data from "./310000.json";
+import data from "./china.json";
 import { createMapDocument } from "./core-2d/MapDocument.ts";
-import { createMultiPolygonLayer, createPointLayer } from "./core-2d/Layer.ts";
-import { createGRView2D, destroyGRView2D } from "./core-2d/MapWindow.ts";
-import { shangHai } from "./310000.ts";
-import { readGeoJson } from "./core-2d/read.ts";
-import { createMultiPolygonFeature, createPointFeature } from "./core-2d/Feature.ts";
-import { GeoJSON, Position } from "./core-2d/type.ts";
+import { readFeatureCollectionAsALayer } from "./core-2d/read.ts";
+import { FeatureCollection } from "./core-2d/type.ts";
+import { createGRView2D } from "./core-2d/MapWindow.ts";
 import { Renderer } from "@antv/g-canvas";
 
 export const App = () => {
 
     function handleClick1() {
         const document = createMapDocument();
-        const pointLayer = createPointLayer();
-        readGeoJson(shangHai as any as GeoJSON);
-        const pointData = shangHai.features.map((value) => {
-            return value.properties.centroid;
-        });
-        pointData.forEach((value) => {
-            pointLayer.addFeature(createPointFeature([value[0], value[1]]));
-        });
-        document.addLayer(pointLayer);
+        const layer = readFeatureCollectionAsALayer(data as any as FeatureCollection);
+
+        // const pointLayer = createPointLayer();
+        // readGeoJson(shangHai as any as GeoJSON);
+        // const pointData = shangHai.features.map((value) => {
+        //     return value.properties.centroid;
+        // });
+        // pointData.forEach((value) => {
+        //     pointLayer.addFeature(createPointFeature([value[0], value[1]]));
+        // });
+        document.addLayer(layer);
 
         // const polygonLayer = new PolygonLayer();
         // let polygonData = shangHai.features.map((value) => {
@@ -32,42 +33,47 @@ export const App = () => {
         // });
         // mapDocument.addLayer(polygonLayer);
 
-        const multiPolygonLayer = createMultiPolygonLayer();
-        let polygonData = shangHai.features.map((value) => {
-            return value.geometry.coordinates;
-        });
-        polygonData.forEach((polygon) => {
-            const position = polygon.map((p) => {
-                return p.map((l) => {
-                    return l.map((position) => {
-                        return position;
-                    });
-                });
-            });
-            console.log(position);
-            multiPolygonLayer.addFeature(createMultiPolygonFeature(position as Position[][][]));
-        });
-        document.addLayer(multiPolygonLayer);
-        console.log(document);
+        // const multiPolygonLayer = createMultiPolygonLayer();
+        // let polygonData = shangHai.features.map((value) => {
+        //     return value.geometry.coordinates;
+        // });
+        // polygonData.forEach((polygon) => {
+        //     const position = polygon.map((p) => {
+        //         return p.map((l) => {
+        //             return l.map((position) => {
+        //                 return position;
+        //             });
+        //         });
+        //     });
+        //     console.log(position);
+        //     multiPolygonLayer.addFeature(createMultiPolygonFeature(position as Position[][][]));
+        // });
+        // document.addLayer(multiPolygonLayer);
+        // console.log(document);
         createGRView2D({
             container: "container",
-            width: 800,
-            height: 500,
+            width: 1200,
+            height: 1000,
             background: "gray",
             renderer: new Renderer()
-        },document);
+        }, document);
+        // document.layers.forEach((layer) => {
+        //     layer.features.forEach((feature) => {
+        //         console.log(feature.properties.name);
+        //     });
+        // });
     }
 
-    function handleClick2() {
-        destroyGRView2D();
-    }
-
-    function handleClick3() {
-    }
-
-    function handleClick4() {
-        console.log();
-    }
+    // function handleClick2() {
+    //     // destroyGRView2D();
+    // }
+    //
+    // function handleClick3() {
+    // }
+    //
+    // function handleClick4() {
+    //     console.log(data);
+    // }
 
 
     return (
@@ -77,9 +83,9 @@ export const App = () => {
             }}>
             </div>
             <button onClick={handleClick1}>create</button>
-            <button onClick={handleClick2}>delete</button>
-            <button onClick={handleClick3}>+</button>
-            <button onClick={handleClick4}>-</button>
+            {/*<button onClick={handleClick2}>delete</button>*/}
+            {/*<button onClick={handleClick3}>+</button>*/}
+            {/*<button onClick={handleClick4}>-</button>*/}
         </>
     );
 };
