2cfc569276cdc41945344991a682b68d8538be63
raingrain <2711835261@qq.com>
2024-01-02 18:26:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,16 +12,21 @@ import {
 export const App = () => {
 
     function handleClickCreate() {
+        // 创建地图文档
         const mapDocument = createMapDocument();
+        // 创建图层并命名
         const layer = readGeoJsonFeatureCollectionAsALayer(China as any as FeatureCollection);
         layer.setName("China");
+        // 将图层加入到地图文档
         mapDocument.addLayer(layer);
+        // 创建地图窗口并渲染
         createMapWindow({
             container: "container",
             width: 600,
             height: 600
         }, mapDocument);
-        downloadGeoJsonFile(createGeoJsonFeatureCollectionAsALayer(mapDocument.layers[0]), layer.name);
+        // 下载图层数据至geojson
+        // downloadGeoJsonFile(createGeoJsonFeatureCollectionAsALayer(mapDocument.layers[0]), layer.name);
     }
 
     function handleClickDestroy() {
